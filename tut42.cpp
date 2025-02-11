cca5d452260d558b8e683c0bc8e3105b835a201a
Talha0646 <awant0837@gmail.com>
2025-02-11 13:01:11 +0500

    Add files via upload

---
+++ tut42.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+/*
+Create 2 classes:
+        1=SimpleCalcutor:
+                        Take input of 2 numbers using a utility function and perfoms
+                         +,-,*,/ and display the results using another function.
+        2=ScientificCalculator:
+                        Take input of 2 numbers using a utility function and perfoms
+                        any four scientific operation of your chioice and display the
+                        results using another function.
+
+Create another class HybridCalculator and inherit it using this 2 classes
+Question 1=What type of inheritance are you using?
+Question 2=Which mood of inheritance are you usiing?
+Question 3=Create an object of HybridCalculator and display reuslts of simple
+and Scientific Calculator?
+Question 4=How is code reusability implemented?                         
+*/
+
+int main()
+{
+
+    return 0;
+}
